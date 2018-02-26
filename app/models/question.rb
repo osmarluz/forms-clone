04912447972b19c4e459cac9c90b91d702485413
osmarluz <osmar.luz@gmail.com>
2018-02-25 22:00:11 -0300

    Update models to add validations and custom methods

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :form
+  has_many :questions_answers, dependent: :destroy
+  
+  validates :title, :type, :form, presence: true
+  
+  enum type: [ :short_text, :long_text, :integer, :boolean ]
 end
